ca41651c5791bcec08e075ce59cc07e8018d48ab
leozartino <leozartino@gmail.com>
2019-05-13 07:17:05 -0300

    add files aula node 12/19

---
+++ node/funcionarios/funcionarios.js
@@ -0,0 +1,34 @@
+const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
+const axios = require("axios");
+const hP = require("./heapSort"); //importando o código do arquivo com o sorting algothim
+const heapSort = hP.heapSort; //chamo isso de gambiarra, atribuindo a fun de ordenação com um nome limpo
+
+//O axios é um client http, ele é usado para obter informações
+//de um servidor, de algo que esta remoto.
+//ele envia uma requisição para um servidor
+
+//1 - Nosso objetivo é obter um arquivo JSON que se encontra na ulr descrita acima
+//utilizando o axios para enviar a request e obter uma response
+//get envia a request, quando a requisição retornar é chamado o .then
+
+//Desafio: Obtenha a mulher chinesa com menor salário do arquivo JSON retornado
+
+axios.get(url).then(response => {
+  const infoFuncionarios = response.data;
+  let chinesaMenorSalario = null;
+  let pessoaMenor = null;
+
+  //obtendo somente as pessoas da china e mulheres, além de pegar só o salario
+  const chinesaSalario = infoFuncionarios
+    .filter(pessoa => pessoa.pais === "China")
+    .filter(pessoa => pessoa.genero === "F");
+  const chSalario = chinesaSalario.map(fun => fun.salario);
+  //chamando o heapSort para ordenar, isso permite que eu retorne o menor valor
+  //pois como é ordenado em ordem crescente, o menor sempre estará na pos de  i = 0;
+  heapSort(chSalario);
+  chinesaMenorSalario = chSalario[0];
+  pessoaMenor = chinesaSalario.filter(
+    funcionario => funcionario.salario === chinesaMenorSalario
+  );
+  return console.log(pessoaMenor);
+});
